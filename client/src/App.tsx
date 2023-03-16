7e6e49410c7244382c868832ebeff7d1937021bd
Alastair Fletcher <alastair.fletcher02@gmail.com>
2023-03-16 13:15:19 +0100

    add CSS and refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,9 +5,9 @@ import './App.css';
 function App() {
   const [influencerData, setInfluencerData] = useState<Influencer[]>([]);
   const [allCategories, setAllCategories] = useState<string[]>([]);
-  const [filteredCategory, setFilteredCategory] = useState<Influencer[]>([]);
+  const [numberOneInCategory, setNumberOneInCategory] = useState<Influencer>();
   const [countries, setCountries] = useState<string[]>([]);
-  const [filteredCountry, setFilteredCountry] = useState<Influencer[]>([]);
+  const [numberOneInCountry, setNumberOneInCountry] = useState<Influencer>();
 
   //====== Fetch and set influencerData
   useEffect(() => {
@@ -42,23 +42,6 @@ function App() {
     // Set countries state
     setCountries(countriesArray);
   }, [influencerData]);
-  //====== Set state of influencers filtered by category
-  function filterByCategory(e: React.ChangeEvent<HTMLSelectElement>) {
-    const category = e.target.value;
-    const influencersByCategory = influencerData.filter(
-      (influencer) =>
-        influencer.category_1 === category || influencer.category_2 === category
-    );
-    setFilteredCategory(influencersByCategory);
-  }
-  //====== Set state of influencers filtered by country
-  function filterByCountry(e: React.ChangeEvent<HTMLSelectElement>) {
-    const country = e.target.value;
-    const influencersByCountry = influencerData.filter(
-      (influencer) => influencer['Audience country(mostly)'] === country
-    );
-    setFilteredCountry(influencersByCountry);
-  }
   //====== Parse numbers from influencer data
   function parseNumbers(str: string) {
     if (str.slice(-1) === 'K') {
@@ -66,48 +49,101 @@ function App() {
     } else str.slice(-1) === 'M';
     return parseFloat(str) * 1000000;
   }
+  //====== Set state of #1 in category by followers
+  function getNumberOneInCategoryByFollowers(
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) {
+    const category = e.target.value;
+    const influencersByCategory = influencerData.filter(
+      (influencer) =>
+        influencer.category_1 === category || influencer.category_2 === category
+    );
+    const numberOneByFollowers = influencersByCategory.reduce((prev, curr) =>
+      parseNumbers(prev.Followers) > parseNumbers(curr.Followers) ? prev : curr
+    );
+    setNumberOneInCategory(numberOneByFollowers);
+  }
+  //====== Set state of #1 in country by engagement average
+  function getNumberOneInCountryByEngagementAverage(
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) {
+    const country = e.target.value;
+    const influencersByCountry = influencerData.filter(
+      (influencer) => influencer['Audience country(mostly)'] === country
+    );
+    const numberOneByEngagementAverage = influencersByCountry.reduce(
+      (prev, curr) =>
+        parseNumbers(prev['Engagement avg\r\n']) >
+        parseNumbers(curr['Engagement avg\r\n'])
+          ? prev
+          : curr
+    );
+    setNumberOneInCountry(numberOneByEngagementAverage);
+  }
 
   return (
     <div className="App">
-      <h1>Influencer data</h1>
+      <h1>Instagram Influencer data</h1>
       <div className="card">
-        <div>
-          <span>The #1 top influencer in the </span>
-          <select
-            data-testid="categories"
-            name="categories"
-            id="categories"
-            onChange={filterByCategory}
-            // className="select select-primary w-full"
-          >
-            {allCategories.map((category) => {
-              return (
-                <option value={category} key={category}>
-                  {category}
-                </option>
-              );
-            })}
-          </select>
-          <span> category is...</span>
+        {/* categories */}
+        <div className="info">
+          <span>
+            The #1 top influencer in the{' '}
+            <select
+              name="categories"
+              id="categories"
+              onChange={getNumberOneInCategoryByFollowers}
+            >
+              {allCategories.map((category) => {
+                return (
+                  <option value={category} key={category}>
+                    {category}
+                  </option>
+                );
+              })}
+            </select>{' '}
+            category, ranked by number of followers, is
+            <span className="influencer">
+              {' '}
+              {numberOneInCategory?.['Influencer insta name']}
+            </span>
+            , with
+            <span className="influencer">
+              {' '}
+              {numberOneInCategory?.['Followers']}{' '}
+            </span>
+            followers.
+          </span>
         </div>
-        <div>
-          <span>The #1 top influencer in </span>
-          <select
-            data-testid="countries"
-            name="countries"
-            id="countries"
-            onChange={filterByCountry}
-            // className="select select-primary w-full"
-          >
-            {countries.map((country) => {
-              return (
-                <option value={country} key={country}>
-                  {country}
-                </option>
-              );
-            })}
-          </select>
-          <span> is...</span>
+        {/* countries */}
+        <div className="info">
+          <span>
+            The #1 top influencer in{' '}
+            <select
+              name="countries"
+              id="countries"
+              onChange={getNumberOneInCountryByEngagementAverage}
+            >
+              {countries.map((country) => {
+                return (
+                  <option value={country} key={country}>
+                    {country}
+                  </option>
+                );
+              })}
+            </select>
+            , ranked by average engagement, is
+            <span className="influencer">
+              {' '}
+              {numberOneInCountry?.['Influencer insta name']}{' '}
+            </span>
+            with an average engagement of
+            <span className="influencer">
+              {' '}
+              {numberOneInCountry?.['Engagement avg\r\n']}
+            </span>
+            .
+          </span>
         </div>
       </div>
     </div>
